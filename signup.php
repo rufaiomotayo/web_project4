8c50579634c8f8352ff280e0efd62fdc283d286f
daniel8961 <54596394+daniel8961@users.noreply.github.com>
2022-05-01 01:45:29 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,12 +9,12 @@
     <link rel="stylesheet" href="signup_login.css">
   </head>
   <body>
-    <h1>Welcome to the Jeopardy!</h1>
+    <h1>Welcome to the Site!</h1>
     
     <div class="enter">
       <form class="" action="signup_submit.php" method="post">
         <div class="enter_input">
-          <p>Sign up to enter the game</p>
+          <p>Sign up </p>
           <div class="input_box">
             <input type="text" name="username" placeholder="Username"><br>
           </div>
