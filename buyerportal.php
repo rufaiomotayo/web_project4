733f6cdb56bcf69cbd516cd9a6bbbaf63bdc79e9
CreatedCell <55263452+CreatedCell@users.noreply.github.com>
2022-05-01 15:15:04 -0400

    Skeletons
    Some files. I didn't add the login stuff cuz you already did. Going to do the portals now. Reupload soon.

---
+++ buyerportal.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html>
+
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Buyer Portal</title>
+	<link href="project4.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<div>
+		<!-- Add the form tag with appropriate attributes -->
+			<form action="login-submit.php" method="post">
+			<h3>Login</h3>
+			<p>Username:<br> <input name="username" type="text"></p>
+			<p>Password:<br> <input name="password" type="text"></p>
+			<input type="submit" id="button2" value="Submit">
+			<!-- Redirect how would you handle user cancel -->
+			<a href="________"><input type="button" id="btn1"  value="Cancel"></a>
+			<!-- Close the form tag -->
+			</form>
+	</div>
+</body>
