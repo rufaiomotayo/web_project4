2c3f0dc9e1c23806a9d79554c2d00824218cfdbd
daniel8961 <54596394+daniel8961@users.noreply.github.com>
2022-05-01 02:31:21 -0400

    Credit card Payment page.
    3 payment page files, html, css, php. Almost done with css, just need to match the main page css. 
    Have not put the php part up. What's inside php file is just scratch.

---
+++ payment_submit.php
@@ -0,0 +1,25 @@
+<?php
+  $card_name = $_POST["card_name"];
+  $card_number = $_POST["card_name"];
+  $expiry_date = $_POST["expiry_date"];
+  $cvv_code = $_POST["cvv_code"];
+  $zipcode = $_POST["zipcode"];
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Payment</title>
+  </head>
+  <body>
+    <!-- <?php
+    if(!isset($card_name)){
+    ?>
+    <p>Enter a valid name on the credit card<br>
+      <a href="payment.html">Back to Payment</a>
+    </p>
+    <?php
+    }
+    ?> -->
+  </body>
+</html>
